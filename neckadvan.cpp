da5eae05c3fb396260311860ae7069366536406b
chain-smok <101109105+chain-smok@users.noreply.github.com>
2022-04-11 10:08:30 +0800

    updated response object

--- neckadvan.cpp
+++ neckadvan.cpp
@@ -2,14 +2,19 @@
 using namespace std;
 int main()
 {
-
-
-
-
-
-
-
-
-
-                    \
+  int vt,v0;
+  while(cin>>vt>>v0){
+    if(vt==0&&v0==0)
+     break;
+    int r=vt%(2*v0);
+    if(r==v0){
+       cout<<0<<endl;
+    }
+    else if(r<v0){
+       cout<<vt/(2*v0)<<endl;
+    }  
+    else{
+       cout<<vt/(2*v0)+1;
+    }
+}
 }
