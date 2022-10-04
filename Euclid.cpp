0d267bdf9f65e813c27d00a64946eb738256959a
chain-smok <101109105+chain-smok@users.noreply.github.com>
2022-10-05 01:51:43 +0800

    Update Euclid.cpp

--- Euclid.cpp
+++ Euclid.cpp
@@ -1,24 +1,24 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <math.h>
 using namespace std;
-int gcd(int a,int b,int *x,int *y){
-  int tx,ty,d;
-  if(b>a)
-    return gcd(b,a,y,x);
-  if(b==0){
-   *x=1;
-   *y=0;
-   return a;
-  }
-  d=gcd(b,a%b,&tx,&ty);
-  *x=ty;
-  *y=tx-floor(a/b)*ty;
-  return d;
+int gcd(int a,int b,int &x,int &y){
+  if(a==0||a==b){
+    x=0;
+    y=1;
+    return b;}
+  else{
+  int xp,yp;
+  int g = gcd(b%a, a, xp, yp);
+  x = yp - b/a * xp, y = xp; // p := previous
+  return g;
 }
-int main()
-{
-  int a,b,x,y,d;
-  while(cin>>a>>b){
-   d=gcd(a,b,&x,&y);
+}
+int main(){
+ int m,n,x,y,d;
+ while(cin>>m>>n){
+   //if(m>n)swap(m,n);
+   int d=gcd(m,n,x,y);
    cout<<x<<" "<<y<<" "<<d<<endl;
-  }
-}
+ }
+} 
