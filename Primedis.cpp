67fffda581516be57160d4f0b2d5ce285eb29388
chain-smok <s1101780@gmail.com>
2022-10-18 16:16:12 +0800

    10.18 CPE PREPARE

--- Primedis.cpp
+++ Primedis.cpp
@@ -1,66 +1,65 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define MAX 46341
-#define TOTAL 4793
-int numofPrime=0;
-int checkPrime[MAX],prime[TOTAL];
-void prime_filter1(){
-  int i=0,j=0;
-  prime[numofPrime++]=2;
-  int MaxCheckValue=sqrt(46341);
-  for(i=3;i<MaxCheckValue;i+=2){
-     if(checkPrime[i-1]==0)
-       prime[numofPrime++]=i;
-     for(j=i*i;j<MAX;j+=2*i)
-        checkPrime[j-1]=1;
-  }
-  for(i=MaxCheckValue;i<MAX;i+=2)
-      if(checkPrime[i-1]==0)
-         prime[numofPrime++]=i;
+#include <cstring>
+#include <cstdio>
+ 
+int visit[50001], prime[50001];
+int makelist(int n)
+{
+	memset(visit, 0, sizeof(visit));
+	int prime_count = 0;
+	for (int i = 2; i < n; ++ i) {
+		if (!visit[i]) {
+			prime[prime_count ++] = i;
+			for (int j = i+i; j < n; j += i) {
+				visit[j] = 1;
+			}
+		}
+	}
+	return prime_count;
 }
-int prime_filter2(int n){
-  int i=0;
-  if(n<MAX)
-     return checkPrime[n-1];
-  for(i=0;i<numofPrime&&i*i<=n;i++)
-    if(n%prime[i]==0)
-      return 1;
-  return 0;
+ 
+int plist[1000001], ans[1000001];
+int calclist(int prime_count, int L, int U)
+{
+	memset(plist, 0, sizeof(plist));
+	for (int i = 0; i < prime_count; ++ i) {
+		int s = (L-1)/prime[i]+1;
+		if (s < 2) s = 2;
+		for (unsigned int j = s*prime[i]; j <= U; j += prime[i]) {
+			plist[j-L] = 1;
+		}
+	}
+	int ans_count = 0;
+	for (unsigned int i = L; i <= U; ++ i) {
+		if (i > 1 && !plist[i-L]) {
+			ans[ans_count ++] = i;
+		}
+	}
+	if (ans_count < 2)
+		puts("There are no adjacent primes.");
+	else {
+		int max = -2147483647, min = 2147483647, mil, mir, mal, mar;
+		for (int i = 1; i < ans_count; ++ i) {
+			if (max < ans[i]-ans[i-1]) {
+				max = ans[i]-ans[i-1];
+				mal = ans[i-1]; 
+				mar = ans[i];
+			}
+			if (min > ans[i]-ans[i-1]) {
+				min = ans[i]-ans[i-1];
+				mil = ans[i-1]; 
+				mir = ans[i];
+			}
+		}
+		printf("%d,%d are closest, %d,%d are most distant.\n",mil,mir,mal,mar);
+	}
 }
-int main(){
-  prime_filter1();
-  long int L=0,U=0;
-  while(scanf("%ld %ld",&L,&U)!=EOF){
-    long int C1=0,C2=0;
-    long int D1=0,D2=0;
-    long int preID=0;    
-    long int max=0,min=MAX+1;
-    long int temp[U-L+2];
-    long int i=0,j=0;
-    if(L<=2){
-      temp[j++]=2;
-      L=3;
-    }else if(L%2==0)
-        L++;
-    for(i=L;i<=U;i+=2)
-      if(!prime_filter2(i))
-         temp[j++]=i;
-    for(i=1;i<j;i++){
-       long int value=temp[i]-temp[i-1];
-       if(value<min){
-           min=value;
-           C1=temp[i-1];
-           C2=temp[i];
-       }
-       if(value>max){
-        max=value;
-        D1=temp[i-1];
-        D2=temp[i];
-       }
-    }
-   if(max==0)
-      printf("There are no adjacent primes.\n");
-   else 
-      printf("%ld,%ld are closet, %ld,%ld are most distant.\n",C1,C2,D1,D2);
-  }
+ 
+int main()
+{
+	int prime_count = makelist(46340);
+	int L, U;
+	while (~scanf("%d%d",&L,&U)) {
+		calclist(prime_count, L, U);
+	}
+    return 0;
 }
