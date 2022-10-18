522c76be7052abf25c36b6c4eae56b2cf6c63c5c
chain-smok <s1101780@gmail.com>
2022-10-18 13:16:42 +0800

    10/18 update

--- cake.cpp
+++ cake.cpp
@@ -1,25 +1,31 @@
-#include <bits/stdc++.h>
+//cherry has 2N.
+//We need to find the line which
+//can half cake.
+//(AX+BY>0)&&(AX+BY<0)=true
+#include <iostream>
 using namespace std;
-int sign(int X)
-{
-  if(X>0)return 1;
-  if(X<0)return -1;
-  if(X==0)return 0;
-}
+typedef struct cake{
+  int x,y;
+}cake;
+cake cherry[100]; 
 int main(){
-  int n,a,b,sx[101],sy[101],k,diff,s;
-  while(cin>>n&&n){
-    for(k=0;k<2*n;k++)cin>>sx[k]>>sy[k];
-    for(a=-500;a<=500;a++){
-        for(b=-500;b<=500;b++){
-         for(diff=k=0;k<2*n;k++){
-            if(s=sign(a*sx[k]+b*sy[k]))diff+=s;
-            else {diff=1;break;}
-         }
-         if(diff)continue;
-         cout<<a<<" "<<b<<endl;
-         a=b=1000;
-        }
+  int N,l,r;
+  while(cin>>N&&N){
+    for(int i=0;i<2*N;i++){
+        cin>>cherry[i].x>>cherry[i].y;
     }
-  }                  
+    int flag=1;
+    for(int A=-500;A<=500&&flag;A++)
+      for(int B=-500;B<=500&&flag;B++){
+        l=0,r=0;
+        for(int i=0;i<2*N;i++){
+          l+=(A*cherry[i].x+B*cherry[i].y<0);
+          r+=(A*cherry[i].x+B*cherry[i].y<0);
+        }
+        if(l==N&&r==N){
+          flag=0;
+          cout<<A<<" "<<B<<endl;
+        }
+      }
+  }
 }
---
+++ simple_div10_13.cpp
@@ -0,0 +1,15 @@
+// n/d=q....r
+// 輸入多個n,尋找共餘的除數
+#include <iostream>
+using namespace std;
+int main(){
+int i=0,a[1000],m;
+while(cin>>a[i]&&a[i]!=0){
+ i++;
+ if(a[i]==0)break;
+ else if(a[i]!=0){
+   
+ }
+}
+if(a[0]!=0)cout<<m;
+}
